fc19c764616e27cbd495e686475049f6fc87878b
FernandoCaccharelli <fer.caccharelli@gmail.com>
2021-11-14 15:22:31 -0300

    subiendo sección de login, register y basket

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.listen(3030, () => { console.log(`Servidor escuchando en el puerto ${3030}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/basket.html'))
+  })
+
+  app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+  })
+
+  app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+  })
