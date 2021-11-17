c6f033f74408ae40b47260044ca063d0865bd05c
sofia joffe <sofia@MacBook-Pro-de-sofia.local>
2021-11-17 00:38:47 -0300

    agregando productDetail y cambios

--- app.js
+++ app.js
@@ -24,4 +24,10 @@ app.get('/basket', (req, res) => {
   app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
   })
+  app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+  })
+  
+
+ 
   
