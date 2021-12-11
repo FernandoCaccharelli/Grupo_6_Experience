ba5fbc8c75b12d91fca20d4e35c744b6482d711f
FernandoCaccharelli <fer.caccharelli@gmail.com>
2021-12-10 23:08:09 -0300

    sistema ruteo /products

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
+
+
 const productsController = {
     products: (req,res)=>{
-        res.render("products/products")
+        
+        res.render("products/products", { productos : products})
     }
 }
 
