3c84aef40d07be1a45a8bd53092ef69e6abba93f
sofia joffe <sofiajo.96@gmail.com>
2021-12-17 00:02:47 -0300

    todo en products

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,13 +5,19 @@ const productsController = require ("../controllers/productsController");
 //lista de productos
 router.get("", productsController.products);
 
+//detalle de producto
+router.get("/:id", productsController.productDetail);
+
 //crear producto
 router.get("/create", productsController.create);
 router.post("/", productsController.store );
 
 //editar producto
-router.get("/edit/:id", productsController.edit);
+router.get("/:id/edit", productsController.edit);
 router.put("/:id",productsController.update);
+
+//borrar producto
+router.delete("/:id", productsController.destroy);
 
 
 
